cf6503e89c979689c9426c9aef77c39c902d9572
Salman Basrai <salmanbasrai98@gmail.com>
2021-05-04 00:34:01 -0400

    added ? -> N script

---
+++ qton.py
@@ -0,0 +1,20 @@
+import sys
+
+## Replace all "?" characters with "N"
+
+## Call this script in the command line with:
+## python3 qton.py <sequencefile>
+
+if len(sys.argv) != 2:
+    print("qton.py takes exactly one argument.\nEnter: python3 qton.py <sequencefile>")
+    quit()
+
+fin = open(sys.argv[1])
+data = fin.read()
+fin.close()
+
+data = data.replace('?', 'N')
+
+fout = open(sys.argv[1], 'w')
+fout.write(data)
+fout.close()
