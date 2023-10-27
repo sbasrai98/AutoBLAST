15d48459d7f0de7ce43befad1a58d2c0c7ede05f
Salman Basrai <sbasrai@ml6694-sbasrai.local>
2023-10-27 13:29:26 -0400

    removed old comments from test file

--- tests/test_parse_BLAST_results.py
+++ tests/test_parse_BLAST_results.py
@@ -7,12 +7,8 @@ from parse_BLAST_results import filter_hits
 class test_filter_hits(unittest.TestCase):
     def setUp(self):
         self.input_file = 'tests/inputs/sample_22_viral_contigs_blasted.txt'
-        # self.player = Player()
-        # self.player.letters = list('BANANAGRAMS')
 
     def test_filter_hits(self):
         self.assertIsInstance(filter_hits(self.input_file, 'Viruses'), pd.DataFrame, 'filter_hits() returns a DataFrame')
-        #self.assertEqual(self.player.can_i_spell('BANANAS'), True, 'word can be spelled')
-
 
 unittest.main()
