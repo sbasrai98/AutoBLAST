f8d27b8b0ab292b2b137a30a12f29d9b6eae6df2
Salman Basrai <sbasrai@ml6694-sbasrai.local>
2023-10-29 23:47:38 -0400

    testing framework set up correctly (use 'python3 -m unittest')

--- tests/test_parse_BLAST_results.py
+++ tests/test_parse_BLAST_results.py
@@ -1,14 +1,14 @@
 import unittest
 import pandas as pd
-from parse_BLAST_results import filter_hits
-#import scripts.parse_BLAST_results
-
+from scripts.parse_BLAST_results import *
 
 class test_filter_hits(unittest.TestCase):
     def setUp(self):
         self.input_file = 'tests/inputs/sample_22_viral_contigs_blasted.txt'
 
     def test_filter_hits(self):
-        self.assertIsInstance(filter_hits(self.input_file, 'Viruses'), pd.DataFrame, 'filter_hits() returns a DataFrame')
-
-unittest.main()
+        parsed_hits = filter_hits(self.input_file, 'Viruses')
+        self.assertIsInstance(parsed_hits, pd.DataFrame, 'filter_hits() returns a DataFrame')
+        self.assertEqual(parsed_hits.shape[0], 70, 'parsed_hits DataFrame has correct number of rows')
+        self.assertEqual(parsed_hits.shape[1], 8, 'parsed_hits DataFrame has correct number of columns')
+        
