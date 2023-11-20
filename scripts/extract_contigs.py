898b7681819535e279e341aedc8f2cb88e1faad3
Salman Basrai <sbasrai@ml6694-sbasrai.local>
2023-11-20 00:32:10 -0500

    finished extract_contigs.py

--- scripts/extract_contigs.py
+++ scripts/extract_contigs.py
@@ -1,21 +1,27 @@
-import re
+# %%
 import argparse
+import pandas as pd
 from Bio import SeqIO
 
-def extract_contigs(parsed_hits: str, contigs: str) ->
+def extract_contigs(parsed_hits: str, contigs: str) -> list:
+    """Extract all contigs of interest from an assembly.
 
+    parsed_hits: path to tab-delimited BLAST hits file
+    contigs: path to contig assembly FASTA file
+
+    Returns a list of SeqRecord objects."""
+
+    assembly = list(SeqIO.parse(contigs, 'fasta'))
+    hits = pd.read_table(parsed_hits)
+    extracted = [x for x in assembly if x.id.split('_')[1] in list(map(str, hits['Contig']))]
+    return extracted
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Enter: python3 "+sys.argv[0]+" <contigs.fa> <contig_type> <output.fa> <list_of_contigs>")
-        quit()
-    
-    fin = open(sys.argv[1])
-    contig_data = fin.read()
-    fin.close()
-    contig_list = sys.argv[4].split(',')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("parsed_hits", help="tab-delimited BLAST hits file")
+    parser.add_argument("contigs", help="contig assembly FASTA file")
+    parser.add_argument("output", help="name of output file to write")
+    args = parser.parse_args()
 
-    if sys.argv[2] == 'nuv': pull_contigs = nuv
-    if sys.argv[2] == 'prime': pull_contigs = prime
-    if sys.argv[2] == 'spade': pull_contigs = spade
-    pull_contigs(sys.argv[3], contig_data, contig_list)
+    extracted_contigs = extract_contigs(args.parsed_hits, args.contigs)
+    SeqIO.write(extracted_contigs, args.output, 'fasta')
